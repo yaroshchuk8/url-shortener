0f081c1b17d8e21df24fd663921f8d26bd6b77c5
yaroshchuk8 <vladonyaroshchuk@gmail.com>
2025-01-13 20:46:14 +0200

    Setup clean architecture project

---
+++ src/UrlShortener.Presentation/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace UrlShortener.Presentation.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
