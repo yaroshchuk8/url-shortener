7ee550e623aece864436036644437f18df8d74d5
yaroshchuk8 <vladonyaroshchuk@gmail.com>
2025-01-14 22:41:25 +0200

    Make Code property unique

--- src/UrlShortener.Infrastructure/Data/AppDbContext.cs
+++ src/UrlShortener.Infrastructure/Data/AppDbContext.cs
@@ -8,4 +8,14 @@ public class AppDbContext : DbContext
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
     
     public DbSet<Url> Urls { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Url>(builder =>
+        {
+            builder.HasIndex(s => s.Code).IsUnique();
+        });
+    }
 }
---
+++ src/UrlShortener.Infrastructure/Migrations/20250114203819_MakeCodePropertyUnique.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UrlShortener.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeCodePropertyUnique : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Urls_Code",
+                table: "Urls",
+                column: "Code",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Urls_Code",
+                table: "Urls");
+        }
+    }
+}
